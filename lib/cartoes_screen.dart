ee9ed1310a01cead964b646bdd66b28dae3970a7
Andrade <luisfelipeandrade@gmail.com>
2024-10-14 17:17:19 -0300

    Version 01

---
+++ lib/cartoes_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CartoesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cartões'),
+      ),
+      body: Center(
+        child: Text('Tela de Cartões'),
+      ),
+    );
+  }
+}
